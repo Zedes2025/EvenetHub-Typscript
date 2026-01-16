c9f67579e75201cbbde8c91a87bc4187f4cb4f19
unknown <ppsplash13@gmail.com>
2026-01-16 10:08:56 +0100

    done merging

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData, useNavigate } from "react-router";
+import EventCard from "../components/UI/EventCard";
 
 export async function loader() {
   const res = await fetch("http://localhost:3001/api/events");
@@ -21,20 +22,10 @@ export default function EventPage() {
   }
 
   return (
-    <div className="m-6 pb-6 grid grid-cols-2 lg:grid-cols-3 gap-y-6 justify-center">
+    <div className=" m-6 pb-6 grid grid-cols-2 lg:grid-cols-3 gap-y-6 justify-center">
       {results.map((result) => {
-        return (
-          <div className="card m-10  bg-stone-100 w-96 shadow-sm ">
-            <div className="card-body">
-              <h2 className="card-title text-blue-950"> {result.title}</h2>
-              <p className="line-clamp-3">{result.description}</p>
-            </div>
-            <div className="card-title  mt-0 mb-6 m-4 p-4">Location: {result.location} </div>
-            <button onClick={() => openEvent(result.id)} className="bg-blue-400 px-4 py-2 rounded shadow hover:bg-amber-500 block text-center">
-              View Details
-            </button>
-          </div>
-        );
+        const [dateSplit, timeFull] = result.date.split("T");
+        return <EventCard key={result.id} title={result.title} date={dateSplit} location={result.location} ViewDetailsBtn={() => openEvent(result.id)} />;
       })}
     </div>
   );
