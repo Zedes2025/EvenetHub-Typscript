f8d6e02c3b2959577e0f783c176bd62c185d0519
Zeinab <zeinabsadjadi@gmail.com>
2026-01-16 22:53:16 +0100

    sign in/out logic done, key persistence done+ a bit of color change

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,11 +2,17 @@ import { Link } from "react-router";
 import { useAuth } from "../../contexts/AuthContext.jsx";
 export default function Header() {
   const { isAuthenticated, user, token, login, logout, register } = useAuth();
-
+  const handleLogout = () => {
+    if (isAuthenticated) {
+      logout(); // clears state + localStorage
+    } else {
+      navigate("/signin");
+    }
+  };
   return (
-    <div className="navbar bg-blue-100 shadow-sm">
+    <div className="navbar bg-stone-300 shadow-sm">
       <div className="navbar-start">
-        <Link to="/" className=" btn-ghost text-2xl font-bold text-blue-800">
+        <Link to="/" className=" btn-ghost text-2xl font-bold text-amber-950">
           EventHub
         </Link>
       </div>
@@ -16,23 +22,23 @@ export default function Header() {
           <button>
             <Link
               to="/create-event"
-              className="btn p-4 m-2 text-blue-800 text-base border-b-slate-600 shadow-sm"
+              className="btn p-4 m-2 text-amber-950 text-base border-b-slate-600 shadow-sm"
             >
               Create Event
             </Link>
           </button>
         )}
-        <button>
+        <button onClick={handleLogout}>
           <Link
             to="/signin"
-            className="btn p-4 m-2 text-blue-800 text-base border-b-slate-600 shadow-sm"
+            className="btn p-4 m-2 text-amber-950 text-base border-b-slate-600 shadow-sm"
           >
             {!isAuthenticated ? "Sign In" : "Sign Out"}
           </Link>
         </button>
 
         {!isAuthenticated && (
-          <button className="btn bg-blue-800 text-base text-gray-300 p-4 m-2">
+          <button className="btn text-amber-950 text-base border-b-slate-600 p-4 m-2">
             <Link to="/signup" className="hover:underline">
               Sign up
             </Link>
--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 const AuthContext = createContext();
 
 export default function AuthProvider({ children }) {
@@ -6,17 +6,38 @@ export default function AuthProvider({ children }) {
   const [token, setToken] = useState(null);
 
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const login = (email, password) => {
-    setIsAuthenticated(true);
-    setUser({ email });
-    setToken("dummy-token");
-  };
 
+  useEffect(() => {
+    const savedToken = localStorage.getItem("token");
+    if (savedToken) {
+      setToken(savedToken);
+      setIsAuthenticated(true);
+    }
+  }, []);
   const logout = () => {
     setIsAuthenticated(false);
     setUser("Guest");
     setToken(null);
+    localStorage.removeItem("token");
   };
+  const login = async (email, password) => {
+    const res = await fetch("http://localhost:3001/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (!res.ok) {
+      throw new Error("Invalid email or password");
+    }
+
+    const data = await res.json(); // { token: "..." }
+    localStorage.setItem("token", data.token);
+    setIsAuthenticated(true);
+  };
+
   const register = (email, password) => {
     return true;
   };
