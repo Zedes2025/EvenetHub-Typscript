e9598c6bcc59e58aac4b39d8c360a387ec68d115
Zeinab <zeinabsadjadi@gmail.com>
2026-01-20 19:30:13 +0100

    homepage changed to data mod

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData, useNavigate } from "react-router";
+import { useLoaderData, Link } from "react-router";
 import EventCard from "../components/UI/EventCard";
 
 export async function loader() {
@@ -15,17 +15,20 @@ export default function EventPage() {
   if (results.length === 0) {
     return <div className="p-4 text-gray-600">No events found.</div>;
   }
-  const navigate = useNavigate();
-
-  function openEvent(result) {
-    navigate(`/events/${result.id}`);
-  }
 
   return (
     <div className=" m-6 pb-6 grid grid-cols-2 lg:grid-cols-3 gap-y-6 justify-center">
       {results.map((result) => {
         const [dateSplit, timeFull] = result.date.split("T");
-        return <EventCard key={result.id} title={result.title} date={dateSplit} location={result.location} ViewDetailsBtn={() => openEvent(result)} />;
+        return (
+          <Link key={result.id} to={`/events/${result.id}`}>
+            <EventCard
+              title={result.title}
+              date={dateSplit}
+              location={result.location}
+            />
+          </Link>
+        );
       })}
     </div>
   );
