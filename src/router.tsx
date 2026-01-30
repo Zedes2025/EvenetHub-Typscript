a7bc34a720abe35c5b572d599021e3f6a01f17e1
Zeinab <zeinabsadjadi@gmail.com>
2026-01-30 08:59:22 +0100

    more .jsx removed from router

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router";
-import AppLayout from "./components/layout/AppLayout.jsx";
+import AppLayout from "./components/layout/AppLayout";
 import HomePage, { loader as eventLoader } from "./pages/HomePage";
 import ErrorPage from "./pages/ErrorPage.jsx";
 import SignIn, { action as signInAction } from "./pages/SignIn";
@@ -9,11 +9,11 @@ import SignUp, { action as signUpAction } from "./pages/SignUp";
 import EventDetailPage, {
   loader as eventDetailLoader,
   action as eventDetailAction,
-} from "./pages/EventDetailPage.jsx";
+} from "./pages/EventDetailPage";
 
-import CreateEvent, { action as createAction } from "./pages/CreateEvent.jsx";
+import CreateEvent, { action as createAction } from "./pages/CreateEvent";
 
-import ProtectedRoute from "./routes/ProtectedRoute.jsx";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 const router = createBrowserRouter([
   {
